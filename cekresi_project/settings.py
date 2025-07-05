c95d229435f39c4274a3b1a7779778c7297144d5
KemedOh <fileahmadsiohgmail.com>
2025-07-05 15:19:19 +0700

    Aplikasi ini dibuat untuk tujuan pembelajaran python

--- cekresi_project/settings.py
+++ cekresi_project/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-eaa6.up.railway.app']
